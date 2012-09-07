2712406e1385a8ec1b841fed06cb5c30c7351b35
Nicolas McCurdy <thenickperson@gmail.com>
2012-09-07 12:31:07 -0400

    Move some more code into postMessage() and correct send button behavior

--- scripts.js
+++ scripts.js
@@ -10,10 +10,25 @@ $(document).bind("keyup keydown", function(e) {
 });
 
 function postMessage() {
-	var message = $("#input").val().replace(/\n/g, "");
-	showMessage(current_uid, message);
-	$("#input").val("");
-	$("#input").focus();
+	//if there is a message in the input box
+	if($("#input").val().replace(/\n/g, "") != "") {
+		//submit a new IM
+		var message = $("#input").val().replace(/\n/g, "");
+		showMessage(current_uid, message);
+		$("#input").val("");
+		$("#input").focus();
+		//switch active user
+		if(options.switch_user_after_posted_message) {
+			switchUser();
+		}
+	}
+	//if the input box is empty
+	else {
+		//switch active user
+		if(options.switch_user_after_blank_message) {
+			switchUser();
+		}
+	}
 }
 
 function showMessage(uid, message) {
@@ -47,21 +62,7 @@ function switchUser() {
 $("#input").keyup(function(e) {
 	//if it was return (without shift)
 	if(e.keyCode == 13 && !shifted) {
-		//if there is a message in the input box
-		if($("#input").val().replace(/\n/g, "") != "") {
-			//submit a new IM
-			postMessage();
-			if(options.switch_user_after_posted_message) {
-				switchUser();
-			}
-		}
-		//if the input box is empty
-		else {
-			//switch active user
-			if(options.switch_user_after_blank_message) {
-				switchUser();
-			}
-		}
+		postMessage();
 	};
 });
 
